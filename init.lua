beb6d3ccffac8a53d6991543a13d23bfe29d1fa6
Alexander Möhring <github@amoehring.com>
2024-04-08 18:52:45 +0200

    moved custom keybindings to custom dir

--- init.lua
+++ init.lua
@@ -190,17 +190,6 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
--- NOTE: my custom keybindings
-
--- Map esc to jk in insetion mode
-vim.keymap.set('i', 'jk', '<esc>')
-
--- Navigate vim and tmux panes with same command
-vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
-vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
-vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
-vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
-
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
