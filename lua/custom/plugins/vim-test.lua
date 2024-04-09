b9f44391de6a5654406063b79344574c46a77c97
Alexander Möhring <github@amoehring.com>
2024-04-09 14:47:13 +0200

    fixed typo in vim-test

--- lua/custom/plugins/vim-test.lua
+++ lua/custom/plugins/vim-test.lua
@@ -3,10 +3,10 @@ return {
   dependencies = {
     '/preservim/vimux',
   },
-  vim.keymap.set('n', 'leader>t', ':TestNearest<CR>'),
-  vim.keymap.set('n', '<leader>', ':TestFile<CR>'),
-  vim.keymap.set('n', '<leader>', ':TestSuite<CR>'),
-  vim.keymap.set('n', '<leader>', ':TestLast<CR>'),
-  vim.keymap.set('n', '<leader>', ':TestVisit<CR>'),
+  vim.keymap.set('n', '<leader>t', ':TestNearest<CR>'),
+  vim.keymap.set('n', '<leader>T', ':TestFile<CR>'),
+  vim.keymap.set('n', '<leader>a', ':TestSuite<CR>'),
+  vim.keymap.set('n', '<leader>l', ':TestLast<CR>'),
+  vim.keymap.set('n', '<leader>g', ':TestVisit<CR>'),
   vim.cmd "let test#strategy = 'vimux'",
 }
