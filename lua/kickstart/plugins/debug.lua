f89f7a6f1ace09a4ee04980a7b29d0ea947792a3
Alexander Möhring <github@amoehring.com>
2024-04-10 05:33:49 +0200

    changed cpp/c/rust debugger from gdb to lldb

--- lua/kickstart/plugins/debug.lua
+++ lua/kickstart/plugins/debug.lua
@@ -28,27 +28,87 @@ return {
     local dap = require 'dap'
     local dapui = require 'dapui'
 
-    dap.adapters.gdb = {
-      type = 'executable',
-      command = 'gdb',
-      args = { '-i', 'dap' },
-    }
+    dap.adapters.codelldb = function(on_adapter)
+      -- This asks the system for a free port
+      local tcp = vim.loop.new_tcp()
+      tcp:bind('127.0.0.1', 0)
+      local port = tcp:getsockname().port
+      tcp:shutdown()
+      tcp:close()
 
-    dap.configurations.c = {
+      -- Start codelldb with the port
+      local stdout = vim.loop.new_pipe(false)
+      local stderr = vim.loop.new_pipe(false)
+      local opts = {
+        stdio = { nil, stdout, stderr },
+        args = { '--port', tostring(port) },
+      }
+      local handle
+      local pid_or_err
+      handle, pid_or_err = vim.loop.spawn('/path/to/lldb', opts, function(code)
+        stdout:close()
+        stderr:close()
+        handle:close()
+        if code ~= 0 then
+          print('codelldb exited with code', code)
+        end
+      end)
+      if not handle then
+        vim.notify('Error running codelldb: ' .. tostring(pid_or_err), vim.log.levels.ERROR)
+        stdout:close()
+        stderr:close()
+        return
+      end
+      vim.notify('codelldb started. pid=' .. pid_or_err)
+      stderr:read_start(function(err, chunk)
+        assert(not err, err)
+        if chunk then
+          vim.schedule(function()
+            require('dap.repl').append(chunk)
+          end)
+        end
+      end)
+      local adapter = {
+        type = 'server',
+        host = '127.0.0.1',
+        port = port,
+      }
+      -- 💀
+      -- Wait for codelldb to get ready and start listening before telling nvim-dap to connect
+      -- If you get connect errors, try to increase 500 to a higher value, or check the stderr (Open the REPL)
+      vim.defer_fn(function()
+        on_adapter(adapter)
+      end, 1000)
+    end
+
+    -- don't forget to compile/build with debug symbols, otherwise it won't work.
+    dap.configurations.cpp = {
       {
-        name = 'Launch',
-        type = 'gdb',
+        name = 'runit',
+        type = 'codelldb',
         request = 'launch',
+
         program = function()
-          return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+          return vim.fn.input('', vim.fn.getcwd(), 'file')
         end,
+
+        args = { '--log_level=all' },
         cwd = '${workspaceFolder}',
-        stopAtBeginningOfMainSubprogram = false,
+        stopOnEntry = false,
+        terminal = 'integrated',
+
+        pid = function()
+          local handle = io.popen 'pgrep hw$'
+          local result = handle:read()
+          handle:close()
+          return result
+        end,
       },
     }
 
-    dap.configurations.cpp = dap.configurations.c
-    dap.configurations.rust = dap.configurations.c
+    dap.configurations.c = dap.configurations.cpp
+    dap.configurations.h = dap.configurations.cpp
+    dap.configurations.rust = dap.configurations.cpp
 
     require('mason-nvim-dap').setup {
       -- Makes a best effort to setup the various debuggers with
@@ -64,6 +124,7 @@ return {
       ensure_installed = {
         -- Update this to ensure that you have the debuggers for the langs you want
         'delve',
+        'codelldb',
       },
     }
 
