524f558b3e9bd59154c78d8aa18e82f24497d631
Alexander Möhring <urgaj@student.kit.edu>
2024-04-27 18:40:26 +0200

    fixed double configuration of hdl_checker

--- init.lua
+++ init.lua
@@ -547,8 +547,6 @@ require('lazy').setup({
         jdtls = {},
         texlab = {},
         svls = {},
-        hdl_checker = {},
-
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
