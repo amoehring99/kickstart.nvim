d9987e144e2fcdbadf8fe385185266b64b12ad33
Alexander Möhring <urgaj@student.kit.edu>
2024-07-04 11:43:09 +0200

    set keymap to toggle git blame

--- lua/custom/plugins/blame.lua
+++ lua/custom/plugins/blame.lua
@@ -1,5 +1,7 @@
 return {
   'FabijanZulj/blame.nvim',
+  -- override current mapping of git blame line to show git blame virtual
+  vim.keymap.set('n', '<leader>gb', '<Cmd>BlameToggle virtual<CR>', { desc = 'Git Blame' }),
   config = function()
     require('blame').setup()
   end,
