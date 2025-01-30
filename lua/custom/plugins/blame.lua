3db7a27a3d7d68b30ffd62f815da7fc3e72d27f3
Alexander Möhring <119799589+amoehring99@users.noreply.github.com>
2025-01-30 17:25:54 +0100

    enhance keybinding describtion

--- lua/custom/plugins/blame.lua
+++ lua/custom/plugins/blame.lua
@@ -1,7 +1,7 @@
 return {
   'FabijanZulj/blame.nvim',
   -- override current mapping of git blame line to show git blame virtual
-  vim.keymap.set('n', '<leader>gb', '<Cmd>BlameToggle virtual<CR>', { desc = 'Git Blame' }),
+  vim.keymap.set('n', '<leader>gb', '<Cmd>BlameToggle virtual<CR>', { desc = '[G]it [B]lame' }),
   config = function()
     require('blame').setup()
   end,
