34722a1f2facb177a66c5a7d58c718554d837ce9
Alexander Möhring <119799589+amoehring99@users.noreply.github.com>
2025-01-30 16:49:02 +0100

    disable copilot by default

--- lua/custom/plugins/copilot.lua
+++ lua/custom/plugins/copilot.lua
@@ -1,3 +1,25 @@
 return {
-  { 'github/copilot.vim' },
+  {
+    'github/copilot.vim',
+    lazy = false, -- Ensure Copilot is always loaded but disabled by default
+    init = function()
+      vim.g.copilot_enabled = false -- Disable Copilot on startup
+    end,
+    keys = {
+      {
+        '<leader>gc',
+        function()
+          local status = vim.fn['copilot#Enabled']()
+          if status == 1 then
+            vim.cmd 'Copilot disable'
+            print 'Copilot Disabled'
+          else
+            vim.cmd 'Copilot enable'
+            print 'Copilot Enabled'
+          end
+        end,
+        desc = '[G]ithub [C]opilot Toggle',
+      },
+    },
+  },
 }
