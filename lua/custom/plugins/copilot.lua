c4444d8428830746c543a2fbeb96f42a796b025e
Alexander Möhring <119799589+amoehring99@users.noreply.github.com>
2025-04-21 18:18:07 +0200

    add blink with copilot lsp

--- lua/custom/plugins/copilot.lua
+++ lua/custom/plugins/copilot.lua
@@ -22,4 +22,21 @@ return {
       },
     },
   },
+  {
+    'saghen/blink.cmp',
+    dependencies = { 'fang2hou/blink-copilot' },
+    opts = {
+      sources = {
+        default = { 'copilot' },
+        providers = {
+          copilot = {
+            name = 'copilot',
+            module = 'blink-copilot',
+            score_offset = 100,
+            async = true,
+          },
+        },
+      },
+    },
+  },
 }
